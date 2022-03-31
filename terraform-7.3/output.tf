56f049d8019b7f1a22e9534bf197bf8ed8fed5e1
PanMonster <panmonster@mail.ru>
2022-03-31 16:09:17 +0300

    output

--- terraform-7.3/output.tf
+++ terraform-7.3/output.tf
@@ -1,29 +1,12 @@
-output "internal_ip_address_web-001" {
-  value       = resource.yandex_compute_instance.vm[0].network_interface.0.ip_address
+output "ip_nat_for_each" {
+  value = toset([
+    for platform_id in yandex_compute_instance.vm_for_each : platform_id.network_interface.0.nat_ip_address
+  ])
 }
 
-output "external_ip_address_web-001" {
-  value       = resource.yandex_compute_instance.vm[0].network_interface.0.nat_ip_address
+output "ip_nat_for_count" {
+  value = [
+    for platform_id in yandex_compute_instance.vm : platform_id.network_interface.0.nat_ip_address
+  ]
 }
-
-/* Расскоментировать для prod
-output "internal_ip_address_web-002" {
-  value       = resource.yandex_compute_instance.vm[1].network_interface.0.ip_address
-}
-
-output "external_ip_address_web-002" {
-  value       = resource.yandex_compute_instance.vm[1].network_interface.0.ip_address
-}
-
-*/
-
-
-output "internal_ip_address_s1" {
-  value       = resource.yandex_compute_instance.vm_for_each["s1"].network_interface.0.ip_address
-}
-
-output "external_ip_address_s1" {
-  value       = resource.yandex_compute_instance.vm_for_each["s1"].network_interface.0.nat_ip_address
-}
-
 
