d05b7f69ec6780ae2b57e43cc89c95fe19a1dc80
PanMonster <panmonster@mail.ru>
2022-03-31 19:07:13 +0300

    output

--- terraform-7.3/output.tf
+++ terraform-7.3/output.tf
@@ -1,7 +1,19 @@
+output "name_nat_for_each" {
+  value = toset([
+    for platform_id in yandex_compute_instance.vm_for_each : platform_id.name
+  ])
+}
+
 output "ip_nat_for_each" {
   value = toset([
     for platform_id in yandex_compute_instance.vm_for_each : platform_id.network_interface.0.nat_ip_address
   ])
+}
+
+output "name_for_count" {
+  value = [
+    for platform_id in yandex_compute_instance.vm : platform_id.name
+  ]
 }
 
 output "ip_nat_for_count" {
@@ -9,4 +21,5 @@ output "ip_nat_for_count" {
     for platform_id in yandex_compute_instance.vm : platform_id.network_interface.0.nat_ip_address
   ]
 }
+
 
